fbc3693ab1124b7d72ad42da05f304520025ce65
Mason Raasch <raasch.mason@gmail.com>
2017-05-12 16:08:51 -0700

    implemented getElementById and getElementsByTagName. halfway implemented getElementsByClassName

--- dominator.js
+++ dominator.js
@@ -3,14 +3,31 @@ const _ = require('underscore');
 
 const getElementById = function(root, id) {
   // Your code here
+  //flatten into an array 
+  //go through the array and check the id attribute of each el
+  //return when you find the match
+
+  const treeArray = flattenTreeToArray(root);
+  for(let i = 0; i < treeArray.length; i++) {
+    if (treeArray[i].id === id) {
+      return treeArray[i];
+    }
+  }
 };
 
 const getElementsByClassName = function(root, className) {
   // Your code here
+  // attribute name we care about: className
+  const treeArray = flattenTreeToArray(root);
+  const elementsWithClassName = _.filter(treeArray, (el) => el.className === className);
+  return elementsWithClassName;
 };
 
 const getElementsByTagName = function(root, tagName) {
   // Your code here
+  const treeArray = flattenTreeToArray(root);
+  const elementsWithTagName = _.filter(treeArray, (el) => el.tagName === tagName);
+  return elementsWithTagName;
 };
 
 module.exports = {
